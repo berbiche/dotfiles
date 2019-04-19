6fef6092f5513c758f51f0aa70bc7229386def9a
Nicolas Berbiche <nic.berbiche@gmail.com>
2019-04-19 15:40:20 -0400

    Add waybar config
    - Add spotify module
    - Change default styling

---
+++ waybar/.config/waybar/modules/spotify.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
+icon=""
+
+if [[ $class =~ paused|playing ]]; then
+  info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
+  if [[ ${#info} > 40 ]]; then
+    info=$(echo $info | cut -c1-40)"..."
+  fi
+  text="$info [$class] $icon"
+elif [[ $class == "stopped" ]]; then
+  text=""
+fi
+
+echo -e "{\"text\":\""$text"\", \"class\":\""$class"\"}"
