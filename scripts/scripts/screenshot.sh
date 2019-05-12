7ed8489169b0b296482b3944703a079f7dddb9a4
Nicolas Berbiche <nic.berbiche@gmail.com>
2019-05-11 21:12:17 -0400

    screenshot.sh: Add usage command and use notify-send.sh

--- scripts/scripts/screenshot.sh
+++ scripts/scripts/screenshot.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+usage="\
+Usage:
+  $(basename "$0") [OPTION...] {selection,screen,everything} - take a screenshot under Wayland
+
+  -?|--help  show this help text
+
+Where:
+  selection   take a screenshot of a range to be selected
+  screen      take a screenshot of the active output
+  everything  take a screenshot of the entire desktop \
+"
+
 output_file="$(xdg-user-dir PICTURES)/screenshots/$(date +'%Y')/$(date +'%m')/$(date +'%Y-%m-%d-%H.%M.%S').png"
+
+
 
 main() {
   case "$1" in
@@ -13,11 +27,12 @@ main() {
     everything)
       print_everything
       ;;
-    -h|--help)
-      echo "Accepted options are: 'selection', 'screen'"
-      ;&
+    -?|--help)
+      echo "$usage" >&2
+      exit
+      ;;
     *)
-      echo 'no screenshot option selected' >&2
+      echo "$usage" >&2
       exit 1
       ;;
   esac
@@ -27,20 +42,31 @@ create_dir() {
   mkdir -p $(dirname "$output_file")
 }
 
+# $1: notification title
+notify() {
+  # Only show if last exit code is a success
+  if [ $? -eq 0 ]; then
+    notify-send.sh --icon=mail-unread --app-name=mail "Screenshot: $1" "Saved to $output_file" --default-action "feh --scale-down --auto-zoom --draw-filename '$output_file'"
+  fi
+}
+
 print_selection() {
   create_dir
   grim -g "$(slurp)" "$output_file"
-  notify-send "Screenshot: " ""
+  notify "selection"
 }
 
 print_active_screen() {
   create_dir
-  grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') "$output_file"
+  local output=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')
+  grim -o $output "$output_file"
+  notify $output
 }
 
 print_everything() {
   create_dir
   grim -c "$output_file"
+  notify "entire desktop"
 }
 
 main "$1"
