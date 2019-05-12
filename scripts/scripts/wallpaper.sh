57ac581e3a9031393e8f653038d551dd827b2372
Nicolas Berbiche <nic.berbiche@gmail.com>
2019-05-11 22:07:41 -0400

    wallpaper.sh: add logging (for the cron job)

--- scripts/scripts/wallpaper.sh
+++ scripts/scripts/wallpaper.sh
@@ -2,10 +2,13 @@
 # Wallpaper downloader using unsplash API
 output_folder="$(xdg-user-dir PICTURES)/wallpaper"
 
-url () {
+get_url () {
   echo "https://source.unsplash.com/featured/$1/?nature,water"
 }
 
 for resolution in '3840x2160' '1920x1080' '1080x1920'; do
-  wget -qO "$output_folder/$resolution.png" "$(url "$resolution")"
+  url=$(get_url "$resolution")
+  output="$output_folder/$resolution.png"
+  echo "fetching resolution $resolution from $url to file $output"
+  wget -qO "$output" "$url"
 done
