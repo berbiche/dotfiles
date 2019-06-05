80e3e9b42ba8bd87d0816dbae0251014bf3ec2f3
Nicolas Berbiche <nic.berbiche@gmail.com>
2019-06-05 01:51:21 -0400

    sway: fix script responsible to reload sway
    The script now also shows a notification before and after reloading.
    
    I don't know whether the command is run in the background subsequently
    the "done reloading" notification might show up before it's actually
    the case.

--- scripts/scripts/sway-reload.sh
+++ scripts/scripts/sway-reload.sh
@@ -3,7 +3,7 @@ INTEGRATED_PANEL='eDP-1'
 
 OUTPUTS=$(swaymsg -t get_outputs | jq 'map({active, name, make, model, serial})')
 INTEGRATED_PANEL_STATE=$(echo $OUTPUTS |
-  jq ".[] | select(.name == \"$INTEGRATED_PANEL\") | if .active then \"enabled\" else \"disabled\" end")
+  jq ".[] | select(.name == \"$INTEGRATED_PANEL\") | if .active then \"enable\" else \"disable\" end")
 MORE_THAN_ONE_OUTPUT=$(echo $OUTPUTS | jq 'length > 0')
 
 # Command to reload sway
@@ -11,10 +11,12 @@ COMMAND="swaymsg 'reload; output $INTEGRATED_PANEL %s'"
 
 # Deactive the integrated display if necessary
 if [[ $MORE_THAN_ONE_OUTPUT == "false" ]]; then
-  COMMAND=$(printf $COMMAND "enabled")
+  COMMAND=$(printf "$COMMAND" "enable")
 else
-  COMMAND=$(printf $COMMAND "$INTEGRATED_PANEL_STATE")
+  COMMAND=$(printf "$COMMAND" "$INTEGRATED_PANEL_STATE")
 fi
 
 # Reload the config
+NOTIFICATION=$(notify-send.sh --print-id "Reloading sway...")
 eval $COMMAND
+notify-send.sh --replace=$NOTIFICATION --expire-time=5000 "Sway done reloading"
