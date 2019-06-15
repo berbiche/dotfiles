35d1fed5d7e2f4429a47eb205f6a6ba7d8d1803e
Nicolas Berbiche <nic.berbiche@gmail.com>
2019-06-15 16:12:42 -0400

    sway: fix integrated panel active state being reinitialized
    The script to handle reloading Sway now uses a sleep
    because _swaymsg_ cannot be run synchronously (as-in lock until Sway is
    done doing it's action).
    
    The sleep duration takes into account my CPU.

--- scripts/scripts/sway-reload.sh
+++ scripts/scripts/sway-reload.sh
@@ -7,7 +7,17 @@ INTEGRATED_PANEL_STATE=$(echo $OUTPUTS |
 MORE_THAN_ONE_OUTPUT=$(echo $OUTPUTS | jq 'length > 0')
 
 # Command to reload sway
-COMMAND="swaymsg 'reload; output $INTEGRATED_PANEL %s'"
+COMMAND="swaymsg reload"
+
+# Reload the config
+NOTIFICATION=$(notify-send.sh --print-id "Reloading sway...")
+eval $COMMAND
+
+# Force sleep, because swaymsg cannot be made to block
+sleep 5
+
+# Now restore the integrated_panel state
+COMMAND="swaymsg 'output $INTEGRATED_PANEL %s'"
 
 # Deactive the integrated display if necessary
 if [[ $MORE_THAN_ONE_OUTPUT == "false" ]]; then
@@ -16,7 +26,6 @@ else
   COMMAND=$(printf "$COMMAND" "$INTEGRATED_PANEL_STATE")
 fi
 
-# Reload the config
-NOTIFICATION=$(notify-send.sh --print-id "Reloading sway...")
 eval $COMMAND
+
 notify-send.sh --replace=$NOTIFICATION --expire-time=5000 "Sway done reloading"
