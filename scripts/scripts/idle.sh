01cc2c04fb88e4bcc2ea5d0ec0b2dcae48980370
Nicolas Berbiche <nic.berbiche@gmail.com>
2019-11-17 01:15:01 -0500

    scripts: update idle.sh to work within nixos

--- scripts/scripts/idle.sh
+++ scripts/scripts/idle.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-# Sway-idle configuration
-/usr/bin/swayidle -w \
-        timeout 300 'swaylock -f' \
-        timeout 600 'swaymsg "output * dpms off"' \
-                resume 'swaymsg "output * dpms on"' \
-        before-sleep 'swaylock -f'
+## Sway-idle configuration
+# This will lock your screen after 300 seconds (5 minutes) of inactivity,
+# then turn off your displays after another 600 seconds (15 minutes total), and
+# turn your screens back on when resumed. It will also lock your screen before
+# your computer goes to sleep.
+#set -x
+
+#swaylock=`command -v swaylock`
+#swayidle=`command -v swayidle`
+#swaymsg=`command -v swaymsg`
+
+swayidle -w \
+    timeout 300 "swaylock -f" \
+    timeout 600 "swaymsg 'output * dpms off'" \
+    resume "swaymsg 'output * dpms on'" \
+    before-sleep "swaylock -f"
