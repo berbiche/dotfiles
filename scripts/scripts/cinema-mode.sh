91b888b1bf51b7c92f3b0bf43f16eed2118a8036
Nicolas Berbiche <nic.berbiche@gmail.com>
2019-11-17 01:17:09 -0500

    scripts: add new script to toggle secondary monitor dpms

---
+++ scripts/scripts/cinema-mode.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#swaymsg output
+PMODEL='BenQ EW3270U'
+PSERIAL='74J08749019'
+
+output=$(swaymsg -t get_outputs | jq '.[] | select((.model == "'"$PMODEL"'") and (.serial == "'"$PSERIAL"'")) | .name')
+
+if [ "$1" = "on" ]; then
+  if [ ! -z $output ]; then
+    swaymsg -t get_outputs | jq '.[] | select(.name != '"$output"' and .active) | .name' | \
+      xargs -r -I {} swaymsg -- output "{}" dpms off
+  fi
+elif [ "$1" = "off" ]; then
+  swaymsg -- output \* dpms on
+else
+  echo 'Specify either "on" or "off"'
+fi
