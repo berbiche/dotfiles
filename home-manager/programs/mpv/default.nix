16c119fd994a02d272378d89aba8c56bbb511ee0
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-06-01 19:43:48 -0400

    mpv: delete umpv fifo/socket automatically

--- home-manager/programs/mpv/default.nix
+++ home-manager/programs/mpv/default.nix
@@ -7,4 +7,6 @@
   };
   xdg.configFile."mpv/mpv.conf".source = ./mpv.conf;
   xdg.configFile."mpv/input.conf".source = ./input.conf;
+  # Delete umpv socket/fifo
+  systemd.user.tmpfiles.rules = [ "r %h/.umpv_fifo" "r %h/.umpv_socket" ];
 }
