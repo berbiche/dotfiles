47ca8db69319793c275c4686aa216cb5547bec66
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-06-17 16:59:20 -0400

    Cleanup trailing whitespace

--- nixos/all-packages.nix
+++ nixos/all-packages.nix
@@ -4,26 +4,26 @@
   environment.systemPackages = with pkgs; [
 
     wget curl aria
-    
+
     colordiff
-    
+
     git rsync
-    
+
     exa fd fzf rofi ripgrep tree bc bat
-    
+
     htop gotop ctop ytop
-    
+
     lsof
-    
+
     nmap
     parted
     ranger stow
-    
+
     traceroute telnet tcpdump whois dnsutils mtr
-        
+
     neovim evince alacritty thunderbird libreoffice kitty
     latest.firefox-nightly-bin
-    
+
     docker-compose
   ];
 }
