dd9e8cf67cfe480ec961689b649418d5beb4a0c3
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-07-03 15:22:32 -0400

    Remove usage of nixpkgs.config.allowUnfree in Steam.nix

--- nixos/graphical/steam.nix
+++ nixos/graphical/steam.nix
@@ -1,14 +1,13 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    (steam.override { 
+    (steam.override {
       extraPkgs = pkgs: [ mono gtk3 gtk3-x11 libgdiplus zlib ];
       # Broken
       # nativeOnly = true;
     })
   ];
 
-  nixpkgs.config.allowUnfree = true;
   hardware.opengl.driSupport32Bit = true;
   hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   hardware.pulseaudio.support32Bit = true;
