5bb2cd8b33a97fb7ecf731923706ee03e9e55b1d
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-07-03 19:41:08 -0400

    Add sway-session.target generation

--- user/home-manager/programs/sway/default.nix
+++ user/home-manager/programs/sway/default.nix
@@ -5,4 +5,12 @@
   xdg.configFile."sway/config".source = ./config;
   xdg.configFile."sway/window-rules.d".source = ./window-rules.d;
   xdg.configFile."sway/config.d".source = ./config.d;
+
+  systemd.user.targets.sway-session = {
+    description = "sway compositor session";
+    documentation = "man:systemd.special(7)";
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+  };
 }
