885fb4e96f97bd4b5984f1897713ee9e4f93e010
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-07-19 22:06:54 -0400

    firefox: "pixel-perfect" trackpad scrolling

--- user/programs/firefox.nix
+++ user/programs/firefox.nix
@@ -160,8 +160,9 @@ in
 {
   # Fix Firefox. See <https://mastransky.wordpress.com/2020/03/16/wayland-x11-how-to-run-firefox-in-mixed-environment/>
   home.sessionVariables = {
-    MOZ_DBUS_REMOTE = 1;
-    MOZ_ENABLE_WAYLAND = 1;
+    MOZ_DBUS_REMOTE = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    MOZ_USE_XINPUT2 = "1";
   };
 
   home.packages = [ pkgs.buku ];
