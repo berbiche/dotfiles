29e99f20409dcb0d925ebeb7ec304cc49af0b4fd
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-07-21 21:13:27 -0400

    Update dependencies, remove krunner patch

--- overlays/krunner.nix
+++
@@ -1,7 +0,0 @@
-final: prev: let
-  # Yeah, this is really hacky since we are not injecting our overlays in nixpkgs-master
-  krunner = (import prev.inputs.master { }).kdeFrameworks.krunner;
-in
-{
-  kdeFrameworks = prev.kdeFrameworks // { inherit krunner; };
-}
