5469cfa6a0080a14621bf9af975540514b1038d7
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-08-16 16:50:25 -0400

    kanshi: add new monitor configuration

--- user/programs/kanshi.nix
+++ user/programs/kanshi.nix
@@ -5,7 +5,7 @@
   xdg.configFile."kanshi/config".text = ''
     {
       output "Dell Inc. DELL U2414H R9F1P55S45FL" mode 1920x1080@60Hz position 0,0 transform 270
-      output "Unknown BenQ EW3270U 74J08749019" mode 3840x2160@60Hz position 1080,0
+      output "Benq Corporation BenQ EW3270U 74J08749019" mode 3840x2160@60Hz position 1080,0
       output "Dell Inc. DELL U2414H R9F1P56N68VL" mode 1920x1080@60Hz position 4920,0 transform 90
       output eDP-1 disable
     }
@@ -13,12 +13,12 @@
     {
 
       output "Dell Inc. DELL U2414H R9F1P55S45FL" mode 1920x1080@60Hz position 0,0 transform 270
-      output "Unknown BenQ EW3270U 74J08749019" mode 3840x2160@60Hz position 1080,0
+      output "Benq Corporation BenQ EW3270U 74J08749019" mode 3840x2160@60Hz position 1080,0
       output eDP-1 disable
     }
 
     {
-      output "Unknown BenQ EW3270U 74J08749019" mode 3840x2160@60Hz position 1080,0
+      output "Benq Corporation BenQ EW3270U 74J08749019" mode 3840x2160@60Hz position 1080,0
       output "Dell Inc. DELL U2414H R9F1P56N68VL" mode 1920x1080@60Hz position 4920,0 transform 90
       output eDP-1 disable
     }
@@ -34,13 +34,20 @@
     }
 
     {
-      output "BenQ Corporation BenQ EW3270U 74J08749019" mode 3840x2160 position 0,0
+      output "BenQ Corporation BenQ EW3270U 74J08749019" mode 3840x2160@60Hz position 0,0
       output eDP-1 disable
     }
 
     {
       output eDP-1 enable
       output * mode 1920x1080
+    }
+
+    {
+      output "Lenovo Group Limited LEN P44w-10 0x00007747" mode 3840x1200@144Hz position 0,800
+      output "BenQ Corporation BenQ EW3270U 74J08749019" mode 3840x2160@60Hz position 3840,0 scale 1.5 transform 90
+      exec swaymsg output '"Lenovo Group Limited LEN P44w-10 0x00007747"' adaptive_sync on, \
+                   output '"BenQ Corporation BenQ EW3270U 74J08749019"' adaptive_sync on
     }
   '';
 }
