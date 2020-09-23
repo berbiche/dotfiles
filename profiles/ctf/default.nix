47a944c2286a638a147c53ad0c144971ffe2c004
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-09-23 19:45:15 -0400

    Attempt at packaging gotools with Ghidra

--- profiles/ctf/default.nix
+++ profiles/ctf/default.nix
@@ -4,10 +4,14 @@
   home-manager.users.${config.my.username} = {
     home.packages = with pkgs; [
       wireshark
-      ghidra-bin
+      ghidra-bin-with-go
       gdb
       pwndbg
       burpsuite
+      binutils
+      volatility
+      p7zip
+      patchelf
     ];
   };
 }
