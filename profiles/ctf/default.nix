18d62792300266290f25c1b82e5919dee91530e6
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-11-26 20:27:15 -0500

    Unimportant cleanups

--- profiles/ctf/default.nix
+++ profiles/ctf/default.nix
@@ -1,20 +1,17 @@
 { config, pkgs, lib, ... }:
 
 {
-  my.home = {
-    home.packages = with pkgs; [
-      wireshark
-      ghidra-bin
-      gdb
-      pwndbg
-      burpsuite
-      binutils
-      volatility
-      p7zip
-      patchelf
-      shellcheck
-      python3Packages.binwalk
-      p7zip
-    ];
-  };
+  my.home.home.packages = with pkgs; [
+    wireshark
+    ghidra-bin
+    gdb
+    pwndbg
+    burpsuite
+    binutils
+    volatility
+    p7zip
+    patchelf
+    shellcheck
+    python3Packages.binwalk
+  ];
 }
