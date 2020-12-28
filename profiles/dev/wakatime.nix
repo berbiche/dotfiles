88f13177af2ae5e8c5210597248a5c1a08861125
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-12-28 17:35:24 -0500

    wakatime: set systemd session variable WAKATIME_HOME

--- profiles/dev/wakatime.nix
+++ profiles/dev/wakatime.nix
@@ -4,6 +4,14 @@ let
   inherit (lib) mkEnableOption types;
   inherit (pkgs.stdenv) isDarwin isLinux;
   cfg = config.profiles.dev.wakatime;
+  configFile = builtins.toFile "wakatime.cfg" ''
+    [settings]
+    hide_file_names = true
+    api_url = https://wakatime.notarock.xyz/api/heartbeat
+    api_key = secret
+    [git]
+    disable_submodules = true
+  '';
 in
 {
   options.profiles.dev.wakatime = {
@@ -11,30 +19,23 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    my.home = { config, lib, ... }: 
+    my.home = { config, lib, ... }:
       let wakatimeCfg = "${config.xdg.configHome}/wakatime"; in
-    lib.mkMerge [
-      (lib.mkIf pkgs.stdenv.isLinux {
-        # Use PAM to have it inside Emacs systemd service (not sure it works)?
-        pam.sessionVariables.WAKATIME_HOME = wakatimeCfg;
-      })
-      {
-        home.sessionVariables.WAKATIME_HOME = wakatimeCfg;
-        home.activation.wakatime = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
-          WAKATIME_HOME="wakatimeCfg"
-          mkdir -p $WAKATIME_HOME
-          if [ ! -f "$WAKATIME_HOME/.wakatime.cfg" ]; then
-            cat >"$WAKATIME_HOME/.wakatime.cfg" <<<'${''
-              [settings]
-              hide_file_names = true
-              api_url = https://wakatime.notarock.xyz/api/heartbeat
-              api_key = secret
-              [git]
-              disable_submodules = true
-            ''}'
-          fi
-        '';
-      }
-    ];
+      lib.mkMerge [
+        (lib.mkIf pkgs.stdenv.isLinux {
+          systemd.user.sessionVariables.WAKATIME_HOME = wakatimeCfg;
+          pam.sessionVariables.WAKATIME_HOME = wakatimeCfg;
+        })
+        {
+          home.sessionVariables.WAKATIME_HOME = wakatimeCfg;
+          home.activation.wakatime = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
+            WAKATIME_HOME="${wakatimeCfg}"
+            mkdir -p "$WAKATIME_HOME"
+            if [ ! -f "$WAKATIME_HOME/.wakatime.cfg" ]; then
+              cp --no-preserve=mode,ownership -T ${configFile} "$WAKATIME_HOME/.wakatime.cfg"
+            fi
+          '';
+        }
+      ];
   };
 }
