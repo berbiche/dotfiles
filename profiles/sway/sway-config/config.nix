650fdd8a664ca0badcb4bb3dcac5b626ff87be01
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-12-30 02:09:58 -0500

    sway: remove old workspace assign to monitor

--- profiles/sway/sway-config/config.nix
+++ profiles/sway/sway-config/config.nix
@@ -4,21 +4,6 @@
 let
   inherit (config.lib.my) callWithDefaults;
 
-  mkSwaybar = { outputs, id ? null }: {
-    id = id;
-    position = "top";
-    mode = "dock";
-    statusCommand = "while date +'%Y-%m-%d %l:%M:%S %p'; do sleep 1; done";
-    trayOutput = "none";
-    fonts = [ "FontAwesome 10" "Terminus 10" ];
-    colors = {
-      statusline = "#FFFFFF";
-      background = "#323232";
-      inactiveWorkspace = { border = "#000000"; background = "#5c5c5c"; text = "#FFFFFF"; };
-    };
-    extraConfig = lib.concatMapStringsSep "\n" (x: "output \"${x}\"") outputs;
-  };
-
   mkCommand = commands: lib.concatStringsSep "; \\\n" commands;
 
   mkFloatingNoBorder = { criteria, extraCommands ? [] }: {
@@ -41,10 +26,6 @@ let
     command = "mark \"_social_${name}\"";
   };
 
-  # Primary outputs
-  OUTPUT-HOME-DELL-RIGHT = "Dell Inc. DELL U2414H R9F1P56N68VL";
-  OUTPUT-HOME-DELL-LEFT  = "Dell Inc. DELL U2414H R9F1P55S45FL";
-  OUTPUT-HOME-BENQ = "Unknown BenQ EW3270U 74J08749019";
   OUTPUT-HOME-DELL = "Dell Inc. DELL U3219Q F9WNWP2";
   OUTPUT-LAPTOP = "eDP-1";
 
@@ -112,9 +93,9 @@ let
     hide_edge_borders --i3 smart_no_gaps
 
     # Set default workspace outputs
-    workspace "${WS5}" output "${OUTPUT-HOME-DELL}" "${OUTPUT-HOME-BENQ}" "${OUTPUT-LAPTOP}"
-    workspace "${WS6}" output "${OUTPUT-HOME-DELL}" "${OUTPUT-HOME-BENQ}" "${OUTPUT-LAPTOP}"
-    workspace "${WS7}" output "${OUTPUT-HOME-DELL-RIGHT}" "${OUTPUT-HOME-DELL-LEFT}" "${OUTPUT-HOME-BENQ}"
+    workspace "${WS5}" output "${OUTPUT-HOME-DELL}"
+    workspace "${WS6}" output "${OUTPUT-HOME-DELL}"
+    # workspace "${WS7}" output ""
 
     # Enable/Disable the output when closing the lid (e.g. when using a dock)
     bindswitch --locked lid:on  output ${OUTPUT-LAPTOP} disable
@@ -187,7 +168,7 @@ let
     assigns = {
       # Games related
       "'${WS5}'" = [
-        { instance = "Steam"; }
+        { instance = "Steam"; } { app_id = "steam"; }
         { app_id = "lutris"; }
       ];
       # Movie related stuff
