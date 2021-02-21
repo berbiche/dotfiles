d1420c23294bf9a84a506bc52054282d582b7b1d
Nicolas Berbiche <nicolas@normie.dev>
2021-02-21 10:24:36 -0500

    gpg: use structured attrs settings

--- profiles/secrets/gpg.nix
+++ profiles/secrets/gpg.nix
@@ -16,43 +16,30 @@ in
           # pinentry-program ${pkgs.pinentry.gnome3}/bin/pinentry
           pinentry-program ${pkgs.pinentry.qt}/bin/pinentry
         '';
-        # Pinentry gnome3 is broken on latest version
+        # Pinentry gnome3 is broken on 2021-02-02
         #services.gpg-agent.pinentryFlavor = "gnome3";
         services.gpg-agent.pinentryFlavor = "qt";
       };
     })
     {
       my.home = { config, ... }: {
-        home.packages = [ pkgs.gnupg ];
-
         services.gpg-agent = {
           enable = false;
           enableSshSupport = false;
         };
 
-        home.file."gnupg" = {
-          target = ".gnupg/gpg.conf";
-          text = ''
-            # Enable smartcard
-            #use-agent
-
-            # Default signing key
-            ${lib.optionalString (config.my.identity.gpgSigningKey != null) ''
-              default-key 0x${config.my.identity.gpgSigningKey}
-              trusted-key 0x${config.my.identity.gpgSigningKey}
-            ''}
-
-            digest-algo SHA256
-            personal-digest-preferences SHA512,SHA384,SHA256,SHA224
-            default-preference-list SHA512,SHA384,SHA256,SHA224,AES256,AES192,AES,CAST5,3DES,BZIP2,ZIP,ZLIB,Uncompressed
-
-            # Show Unix timestamps
-            fixed-list-mode
-            #Long hexadecimal key format
-            keyid-format 0xlong
-          '';
+        programs.gpg = {
+          enable = true;
+          settings = let
+            inherit (config.my.identity) gpgSigningKey;
+            hasGpgSigningKey = gpgSigningKey != null;
+          in {
+            use-agent = false;
+            default-key = lib.mkIf hasGpgSigningKey "0x${gpgSigningKey}";
+            trusted-key = lib.mkIf hasGpgSigningKey "0x${gpgSigningKey}";
+          };
         };
       };
-    }
+    } #
   ];
 }
