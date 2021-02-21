731ceecebc0035f1d3ce8ccd1ce5087508961d93
Nicolas Berbiche <nicolas@normie.dev>
2021-02-21 10:26:00 -0500

    wireguard: lowercase message in activation script

--- profiles/wireguard/default.nix
+++ profiles/wireguard/default.nix
@@ -7,7 +7,7 @@
 
   system.activationScripts.configure-wireguard-permissions = ''
     mkdir -p /private/wireguard
-    echo "Setting Wireguard folder permissions"
+    echo "setting Wireguard folder permissions"
     chmod -c 0755 /private /private/wireguard
     chmod -c 0440 /private/wireguard/*
     chown -cR root:systemd-network /private/wireguard
