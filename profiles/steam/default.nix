1bae2b79ec3537df718b1ecb9a19212a9e704f09
Nicolas Berbiche <nicolas@normie.dev>
2021-05-29 20:51:57 -0400

    steam: automatically fetch proton-ge-custom

--- profiles/steam/default.nix
+++ profiles/steam/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 # https://github.com/NixOS/nixpkgs/pull/86480
 {
@@ -13,5 +13,23 @@
     })
   ];
 
-  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux;
+    [ libva ]
+    ++ lib.optionals config.services.pipewire.enable [ pipewire ];
+
+  home-manager.sharedModules = let
+    version = "6.9-GE-2";
+    proton-ge = fetchTarball {
+      url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/Proton-${version}.tar.gz";
+      sha256 = "0y8vv1lzbcbk328fn7kp79gn6k1jaj8yaxkd26vf5xnrncln71d7";
+    };
+  in [
+    {
+      home.file.proton-ge-custom = {
+        recursive = true;
+        source = proton-ge;
+        target = ".steam/root/compatibilitytools.d/Proton-${version}";
+      };
+    }
+  ];
 }
