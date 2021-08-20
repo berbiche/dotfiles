18ceb19c4e8109f6e850fd4220146e5c1aae74ce
Nicolas Berbiche <nicolas@normie.dev>
2021-08-19 23:41:43 -0400

    wireguard: update tq-rs configuration

--- profiles/wireguard/tq-rs.nix
+++ profiles/wireguard/tq-rs.nix
@@ -32,7 +32,8 @@ in
         PrivateKeyFile = "/private/wireguard/zion.key";
       };
       wireguardPeers = map (x: { wireguardPeerConfig = x; }) [{
-        AllowedIPs = [ "10.10.10.0/24" "192.168.0.0/24" "fc00:23:6::/64" ];
+        # AllowedIPs = [ "10.10.10.0/24" "10.97.1.0/24" "10.97.42.0/24" "10.36.25.0/24" "fc00:23:6::/64" ];
+        AllowedIPs = [ "10.10.10.0/24" "10.97.42.0/24" "10.36.25.0/24" "fc00:23:6::/64" ];
         Endpoint = "dozer.qt.rs:51820";
         PersistentKeepalive = 25;
         PresharedKeyFile = "/private/wireguard/zion.preshared";
@@ -42,22 +43,27 @@ in
     systemd.network.networks.${network} = {
       enable = true;
       name = network;
-      dns = [ "192.168.0.6" "10.10.10.2" ];
+      dns = [ "10.97.42.6" "10.10.10.2" ];
       matchConfig.Name = network;
       networkConfig = {
         Address = cfg.ipv4Address;
-        Domains = [ "~tq.rs." "~kifinti.lan." ];
+        Domains = [ "~tq.rs." "lan." ];
         DNSSEC = false;
       };
       routes = map (x: { routeConfig = x; }) [
+        # {
+        #   Gateway = "10.10.10.1";
+        #   Destination = "10.97.1.0/24";
+        #   GatewayOnLink = true;
+        # }
         {
           Gateway = "10.10.10.1";
-          Destination = "192.168.0.0/24";
+          Destination = "10.97.42.0/24";
           GatewayOnLink = true;
         }
         {
           Gateway = "10.10.10.1";
-          Destination = "192.168.42.0/24";
+          Destination = "10.36.25.0/24";
           GatewayOnLink = true;
         }
         {
