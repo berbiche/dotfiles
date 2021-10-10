17d615c851c41c3c1e9698b02273a380c4785dca
Nicolas Berbiche <nicolas@normie.dev>
2021-10-10 12:52:25 -0400

    noisetorch: only load after pipewire/pulseaudio

--- profiles/graphical-linux/default.nix
+++ profiles/graphical-linux/default.nix
@@ -69,6 +69,11 @@
     systemd.user.services.noisetorch = {
       Unit = {
         Description = "noisetorch oneshot loading of microphone suppressor";
+        After = lib.optionals config.profiles.pipewire.enable [ "pipewire.service" ]
+          ++ lib.optionals config.hardware.pulseaudio.enable [ "pulseaudio.service" ];
+        Requisite = lib.optionals config.profiles.pipewire.enable [ "pipewire.service" ]
+          ++ lib.optionals config.hardware.pulseaudio.enable [ "pulseaudio.service" ];
+        PartOf = [ "graphical-session.target" ];
       };
       Service = {
         Type = "oneshot";
