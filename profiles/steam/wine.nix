571e11423e21fd37a1f38a76719f3c97e00cf5c8
Nicolas Berbiche <nicolas@normie.dev>
2021-11-09 17:42:08 -0500

    steam: fix winetricks after flake update

--- profiles/steam/wine.nix
+++ profiles/steam/wine.nix
@@ -6,12 +6,11 @@ let
   cfg = config.profiles.steam.wine;
 
   wine-staging = pkgs.wineWowPackages.staging;
-  winetricks-staging = pkgs.winetricks.override { wine = wine-staging; };
 in
 {
   options.profiles.steam.wine.enable = mkEnableOption "installing wine alongside Steam";
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ wine-staging winetricks-staging ];
+    environment.systemPackages = [ wine-staging pkgs.winetricks ];
   };
 }
