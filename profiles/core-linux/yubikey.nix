2fbdae9e7a16ad975e7c313be531fd1c56be896b
Nicolas Berbiche <nicolas@normie.dev>
2021-11-17 03:23:57 -0500

    yubikey: switch to u2f pam, only enable for sudo, su, etc.

--- profiles/core-linux/yubikey.nix
+++ profiles/core-linux/yubikey.nix
@@ -1,9 +1,30 @@
 { config, pkgs, lib, ... }:
 
 {
-  security.pam.yubico.enable = true;
-  security.pam.yubico.control = "sufficient";
-  security.pam.yubico.mode = "challenge-response";
+  security.pam.u2f.enable = true;
+  security.pam.u2f.interactive = false;
+  security.pam.u2f.cue = true;
+  security.pam.u2f.control = "sufficient";
+  security.pam.u2f.appId = "pam://${config.networking.hostName}";
+
+  # Disable u2fAuth for all these modules
+  # I only want to enable u2fAuth for ssh, sudo and su really
+  security.pam.services = {
+    chpasswd.u2fAuth = false;
+    i3lock.u2fAuth = false;
+    i3lock-color.u2fAuth = false;
+    lightdm.u2fAuth = false;
+    lightdm-autologin.u2fAuth = false;
+    lightdm-greeter.u2fAuth = false;
+    login.u2fAuth = false;
+    passwd.u2fAuth = false;
+    runuser.u2fAuth = false;
+    runuser-l.u2fAuth = false;
+    swaylock.u2fAuth = false;
+    xlock.u2fAuth = false;
+    vlock.u2fAuth = false;
+    xscreensaver.u2fAuth = false;
+  };
 
   home-manager.sharedModules = [{
     # Get notifications when waiting for my yubikey
