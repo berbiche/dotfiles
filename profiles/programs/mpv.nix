2be12b104f2f1183ebe156ebdda41cfdfaa84e3d
Nicolas Berbiche <nicolas@normie.dev>
2021-12-13 01:45:52 -0500

    mpv: cleanup config

--- profiles/programs/mpv.nix
+++ profiles/programs/mpv.nix
@@ -3,89 +3,102 @@
 let
   inherit (pkgs.stdenv.targetPlatform) isDarwin isLinux;
 in
-lib.mkIf isLinux {
+{
   programs.mpv = {
     enable = true;
-    scripts = [ pkgs.mpvScripts.mpris ];
+
+    scripts = lib.mkIf isLinux [ pkgs.mpvScripts.mpris ];
+
+    defaultProfiles = lib.mkDefault [ "gpu-hq" "interpolation" ];
+
+    config = {
+      # Use hardware acceleration
+      hwdec = "vaapi";
+      vo = "gpu";
+      hwdec-codecs = "all";
+      gpu-context = "wayland";
+      keep-open = true;
+
+      script-opts="ytdl_hook-ytdl_path=${pkgs.yt-dlp}/bin/yt-dlp";
+    };
+
+    bindings = {
+      "AXIS_DOWN" = "add volume -2";
+      "AXIS_UP" = "add volume 2";
+
+      "MBTN_MID" = "cycle pause";
+      "MBTN_BACK" = "add chapter -1";
+      "MBTN_FORWARD" = "add chapter 1";
+
+      "r" = "playlist-shuffle";
+    };
+
+    profiles = {
+      interpolation = {
+        interpolation = true;
+        tscale = "box";
+        tscale-window = "quadric";
+        tscale-clamp = 0.0;
+        tscale-radius = 1.025;
+        video-sync = "display-resample";
+        blend-subtitles = "video";
+      };
+
+      profile-thinkpad.profile = "gpu-hq";
+
+      profile-make-my-gpu-scream = {
+        profile = "gpu-hq";
+        interpolation = true;
+        scale = "ewa_lanczossharp";
+        cscale = "ewa_lanczossharp";
+        video-sync = "display-resample";
+        tscale = "oversample";
+      };
+
+      onetime = {
+        keep-open = false;
+      };
+
+      nodir = {
+        sub-auto = false;
+        audio-file-auto = false;
+      };
+
+      image = {
+        profile = "nodir";
+        mute = true;
+        scale = "ewa_lanczossharp";
+        background = 0.1;
+        video-unscaled = true;
+        title="\${?media-title:\${media-title}}\${!media-title:No file}";
+        image-display-duration = "inf";
+        loop-file = true;
+        term-osd = "force";
+        osc = false;
+        osd-level = 1;
+        osd-bar = false;
+        osd-on-seek = false;
+        osd-scale-by-window = false;
+      };
+      "extension.webm" = {
+        loop-file = "inf";
+      };
+      "extension.mp4" = {
+        loop-file = "inf";
+      };
+      "extension.gif" = {
+        interpolation = "no";
+      };
+      "extension.png" = {
+        video-aspect = "no";
+      };
+      "extension.jpg" = {
+        video-aspect = "no";
+      };
+      "extension.jpeg" = {
+        profile = "extension.jpg";
+      };
+    };
   };
-
-  xdg.configFile."mpv/mpv.conf".text = ''
-    # Use hardware acceleration
-    hwdec=vaapi
-    vo=gpu
-    hwdec-codecs=all
-    gpu-context=wayland
-    keep-open=yes
-    profile=gpu-hq
-
-    script-opts=ytdl_hook-ytdl_path=${pkgs.yt-dlp}/bin/yt-dlp
-
-    [profile-thinkpad]
-    profile=gpu-hq
-
-    [profile-make-my-gpu-scream]
-    profile=gpu-hq
-    scale=ewa_lanczossharp
-    cscale=ewa_lanczossharp
-    video-sync=display-resample
-    interpolation
-    tscale=oversample
-
-    [onetime]
-    keep-open=no
-
-    [nodir]
-    sub-auto=no
-    audio-file-auto=no
-
-    [image]
-    profile=nodir
-    mute=yes
-    scale=ewa_lanczossharp
-    background=0.1
-    video-unscaled=yes
-    title="''${?media-title:''${media-title}}''${!media-title:No file}"
-    image-display-duration=inf
-    loop-file=yes
-    term-osd=force
-    osc=no
-    osd-level=1
-    osd-bar=no
-    osd-on-seek=no
-    osd-scale-by-window=no
-
-
-    #load-unsafe-playlists=yes
-
-    [extension.webm]
-    loop-file=inf
-
-    [extension.mp4]
-    loop-file=inf
-
-    [extension.gif]
-    interpolation=no
-
-    # Ignore aspect ratio information for PNG and JPG, because it's universally bust
-    [extension.png]
-    video-aspect=no
-
-    [extension.jpg]
-    video-aspect=no
-
-    [extension.jpeg]
-    profile=extension.jpg
-  '';
-
-  xdg.configFile."mpv/input.conf".text = ''
-    AXIS_DOWN add volume -2
-    AXIS_UP   add volume 2
-
-    MBTN_MID     cycle pause
-    MBTN_BACK    add chapter -1
-    MBTN_FORWARD add chapter 1
-
-    r playlist-shuffle
-  '';
 }
 
