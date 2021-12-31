e79b01e06212335780779f31f1a8f370f8e03f08
Nicolas Berbiche <nicolas@normie.dev>
2021-12-30 21:24:36 -0500

    linux: move user tmpdir to /tmp/user/$(id -u)/
    TMPDIR was aliased `/run/user/$(id -u)` (tmpfs) and would sometime run
    out of space because it's a RAM device.

--- profiles/core-linux/default.nix
+++ profiles/core-linux/default.nix
@@ -21,27 +21,28 @@
   # errors when trying to hibernate, even though the swapfile is as big as
   # the amount of ram I have...
   systemd.tmpfiles.rules = [
-    "w /sys/power/image_size - - - - 0"
+    #Type Path                  Mode User Group Age Argument
+    "w    /sys/power/image_size -    -    -     -   0"
+    "d    %T/user               0777 0    0     -   -"
   ];
 
   # Set default dns servers
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" "1.0.0.1" "8.8.4.4" "9.9.9.9" ];
 
-  # Add a folder in $XDG_RUNTIME_DIR to be used as my own temporary directory
+  # Add a folder in $TMP/user/$UID to be used as my own temporary directory
   home-manager.sharedModules = [
     (let
       tmpdirs = rec {
-        TMP = "\${XDG_RUNTIME_DIR:-/run/user/$(id -u)}/tmp";
+        TMP = "/tmp/user/$(id -u)/";
         TEMP = TMP;
         TMPDIR = TMP;
         TEMPDIR = TMP;
       };
     in {
       systemd.user.tmpfiles.rules = [
-        #Type Path   Mode User Group Age Argument
-        "D    %t/tmp 0770 -    -     -   -"
+        #Type Path         Mode User Group Age Argument
+        "D    /tmp/user/%U 0770 -    -     -   -"
       ];
-
       systemd.user.sessionVariables = tmpdirs;
       home.sessionVariables = tmpdirs;
     })
