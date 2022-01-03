96f1490cdf661ecd33a01bf803d6d018ed0e5730
Nicolas Berbiche <nicolas@normie.dev>
2022-01-02 21:22:55 -0500

    swaync: remove local patches

--- overlays/swaynotificationcenter/default.nix
+++ overlays/swaynotificationcenter/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub
+{ lib, stdenv, fetchFromGitHub, fetchpatch
 , meson, ninja, pkg-config, vala
 , gtk3, glib, gtk-layer-shell
 , dbus, dbus-glib, json-glib, librsvg
@@ -7,18 +7,31 @@
 
 stdenv.mkDerivation {
   pname = "SwayNotificationCenter";
-  version = "unstable-2021-07-21";
+  version = "unstable-2021-12-28";
 
   src = fetchFromGitHub {
     owner = "ErikReider";
     repo = "SwayNotificationCenter";
-    rev = "75894f50ff49b31e9024df3fe6ce97f09f6eb6ad";
-    hash = "sha256-yZ0Kqcfk67fa/Yl9N60h7hHbyTbS+iV8u1y2pu8zYYA=";
+    rev = "c1eb0525601a5af52fadbf8d20a3522492e25aad";
+    hash = "sha256-xo+M8dpqs2tbWQn49PR5ms3TlzMsv9SvY/QN7Jb/MT4=";
   };
 
   patches = [
-    ./dont-bind-gtk-layer-shell-bottom.patch
-    ./ellipsize-content.patch
+    # (fetchpatch {
+    #   name = "ellipsize-content.patch";
+    #   url = "https://github.com/berbiche/SwayNotificationCenter/commit/9b1f15503a648df656d39e2baed3e4be680383a9.patch";
+    #   sha256 = "sha256-S2bVB7FkmNxQ15r0aTlfT8/059E4J8XDiMER6U3K4Vg=";
+    # })
+    (fetchpatch {
+      name = "configurable-notification-center-margins.patch";
+      url = "https://github.com/berbiche/SwayNotificationCenter/commit/81c3eddb1389cdbb32e719a8f81410f9c9034f10.patch";
+      sha256 = "sha256-IRkAq9RIRs1W0TYP8ZRaucYfNACd9MQOswLL3QHizJg=";
+    })
+    # (fetchpatch {
+    #   name = "configurable-notification-center-width.patch";
+    #   url = "https://github.com/berbiche/SwayNotificationCenter/commit/a28fd1b22a6ed337aeff8914337aa91ae594402b.patch";
+    #   sha256 = "sha256-+SZuGfuOCI7jg2atMDozD0ZERRG4UIadXPhkik0VRhs=";
+    # })
   ];
 
   nativeBuildInputs = [ meson ninja pkg-config vala gobject-introspection wrapGAppsHook ];
