e3466fc14419db1a745585c5b54ade697ab7649b
Nicolas Berbiche <nicolas@normie.dev>
2022-01-10 22:32:39 -0500

    emacs: fix highlight font face, trailing whitespace highlighting
    ... and Ivy posframe resize

--- profiles/dev/home-manager/emacs/doom.d/config.el
+++ profiles/dev/home-manager/emacs/doom.d/config.el
@@ -33,7 +33,10 @@
 (global-visual-line-mode t)
 
 ;; Override line highlighting foreground
-(set-face-foreground 'hl-line 'unspecified)
+(add-hook 'server-after-make-frame-hook
+          (lambda ()
+            (set-face-foreground 'highlight 'unspecified)))
+(set-face-foreground 'highlight 'unspecified)
 
 ;; Disable clipboard manager hanging for a few seconds on Wayland
 (setq x-select-enable-clipboard-manager nil)
@@ -68,11 +71,15 @@
   "Toggle `show-trailing-whitespace'"
   (interactive)
   (setq show-trailing-whitespace (not show-trailing-whitespace)))
-
-(add-hook 'vterm-mode-hook (lambda ()
-                             (set (make-local-variable 'show-trailing-whitespace) nil)))
-(add-hook 'term-mode-hook (lambda ()
-                            (set (make-local-variable 'show-trailing-whitespace) nil)))
+;; but disable trailing whitespace for the modes
+(dolist (hook '(vterm-mode-hook
+                term-mode-hook
+                special-mode-hook
+                comint-mode-hook
+                compilation-mode-hook
+                minibuffer-mode-hook))
+  (add-hook hook (lambda ()
+    (set (make-local-variable 'show-trailing-whitespace) nil))))
 
 ;; Lets drag stuff aroung using hjkl
 (map! :ne "C-S-k" #'drag-stuff-up)
@@ -90,24 +97,31 @@
 (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-top-center))
       ivy-posframe-height-alist '((t . 10))
       ivy-posframe-parameters '((internal-border-width . 5)))
-;;(setq ivy-posframe-border '((t (:background "#61BFFF"))))
-(setq ivy-posframe-width 100)
+(setq ivy-posframe-border '((t (:background "#61BFFF"))))
+;; (setq ivy-posframe-width (max 100 (- (frame-width) 10)))
 (ivy-posframe-mode +1)
 
+;; This is also called for ivy-posframe's size change ...
+(add-to-list 'window-size-change-functions
+             (lambda (frame)
+               "Set ivy-posframe-width to a max width of 140 columns on frame resize."
+               (let* ((ivy-frame (buffer-local-value 'posframe--frame (get-buffer ivy-posframe-buffer)))
+                      (width (frame-width))
+                      ;; Don't leave space on the sides when there is less than 70 columns
+                      (min-width (max (min 70 width) (- width 10))))
+                 (when (not (equal ivy-frame frame))
+                   (setq ivy-posframe-width (min 140 min-width))))))
+
 ;; Don't push a new buffer when navigating with RETURN in dired
-;;(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+;; Doesn't work
+;; (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+;; (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))
 
 ;; On vsplit using V, focus the new frame
 (map! :ne "SPC w V" (lambda () (interactive)(evil-window-vsplit) (other-window 1)))
 
 (add-hook 'yaml-mode-hook 'electric-indent-local-mode)
 
-;; (add-hook 'erlang-mode-hook 'linum-mode)
-;; (add-hook 'erlang-mode-hook 'column-number-mode)
-;; (add-hook 'erlang-mode-hook 'which-key-mode)
-
-
-;; Set initial position
 (setq frame-resize-pixelwise t)
 
 (defun indent-buffer ()
