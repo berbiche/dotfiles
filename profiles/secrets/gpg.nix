a355b6aa02e8ae1d7a31cc4a298f34365b7739db
Nicolas Berbiche <nicolas@normie.dev>
2022-02-04 00:19:00 -0500

    gpg: split configuration in home-manager.nix file

--- profiles/secrets/gpg.nix
+++ profiles/secrets/gpg.nix
@@ -1,53 +1,13 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, isLinux, ... }:
 
-let
-  inherit (lib) mkIf mkMerge mkSinkUndeclaredOptions optionalAttrs;
-  inherit (pkgs.stdenv.hostPlatform) isLinux isDarwin;
-in
 {
-  # options.services.dbus = mkIf isDarwin (mkSinkUndeclaredOptions { });
-
-  config = mkMerge [
-    (mkIf isLinux {
+  config = lib.mkMerge [
+    {
+      my.home.imports = [ ./home-manager.nix ];
+    }
+    (lib.optionalAttrs isLinux {
       # Pinentry configuration for gpg-agent with pinentry-gnome3
       services.dbus.packages = [ pkgs.gcr ];
-      my.home = {
-        # gnome3 pinentry unbroken in my Sway setup with
-        # dbus-update-activation-environment from 'on-startup-shutdown' script
-        # home.file.".gnupg/gpg-agent.conf".text = lib.mkAfter ''
-        #   pinentry-program ${pkgs.pinentry.gnome3}/bin/pinentry
-        #   # pinentry-program ${pkgs.pinentry.qt}/bin/pinentry
-        # '';
-        services.gpg-agent = {
-          pinentryFlavor = "gnome3";
-          enable = true;
-          enableSshSupport = true;
-        };
-      };
     })
-    (mkIf isDarwin {
-      # pinentry-mac is not packaged on nixpkgs
-      my.home.home.file.".gnupg/gpg-agent.conf".text = lib.mkAfter ''
-        pinentry-program /usr/local/bin/pinentry-mac
-      '';
-    })
-    {
-      my.home = { config, ... }: {
-        services.gpg-agent = {
-          enable = lib.mkDefault false;
-        };
-
-        programs.gpg = {
-          enable = true;
-          settings = let
-            inherit (config.my.identity) gpgSigningKey;
-            hasGpgSigningKey = gpgSigningKey != null;
-          in {
-            default-key = mkIf hasGpgSigningKey "0x${gpgSigningKey}";
-            trusted-key = mkIf hasGpgSigningKey "0x${gpgSigningKey}";
-          };
-        };
-      };
-    } #
   ];
 }
---
+++ profiles/secrets/home-manager.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (pkgs.stdenv.hostPlatform) isLinux isDarwin;
+in
+lib.mkMerge [
+  {
+    # home.file.".gnupg/gpg-agent.conf".text = lib.mkAfter ''
+    #   pinentry-program ${pkgs.pinentry.gnome3}/bin/pinentry
+    #   # pinentry-program ${pkgs.pinentry.qt}/bin/pinentry
+    # '';
+    services.gpg-agent = lib.mkIf isLinux {
+      pinentryFlavor = "gnome3";
+      enable = true;
+      enableSshSupport = true;
+    };
+
+    programs.gpg = {
+      enable = true;
+      settings = let
+        inherit (config.my.identity) gpgSigningKey;
+        hasGpgSigningKey = gpgSigningKey != null;
+      in {
+        default-key = lib.mkIf hasGpgSigningKey "0x${gpgSigningKey}";
+        trusted-key = lib.mkIf hasGpgSigningKey "0x${gpgSigningKey}";
+      };
+    };
+  }
+
+  (lib.mkIf isDarwin {
+    # pinentry-mac is not packaged on nixpkgs
+    home.file.".gnupg/gpg-agent.conf".text =  lib.mkAfter ''
+      pinentry-program /usr/local/bin/pinentry-mac
+    '';
+  })
+]
