8448401dbb6fab0fcf19d2dd52c1563a0956d101
Nicolas Berbiche <nicolas@normie.dev>
2022-02-18 17:44:24 -0500

    avizo: update package version

--- overlays/avizo.nix
+++ overlays/avizo.nix
@@ -1,3 +1,11 @@
 final: prev: {
-  avizo = final.master.avizo;
+  avizo = prev.avizo.overrideAttrs (drv: {
+    src = prev.fetchFromGitHub {
+      owner = "misterdanb";
+      repo = "avizo";
+      rev = "d5c416aa7ac46660cd943f01ecfab6e29cb5e8c3";
+      hash = "sha256-BRtdCOBFsKkJif/AlnF7N9ZDcmA+878M9lDQld+SAgo=";
+    };
+    patches = [ ];
+  });
 }
--- profiles/sway/home-manager/avizo.nix
+++ profiles/sway/home-manager/avizo.nix
@@ -13,7 +13,9 @@
       block-spacing = 0;
       background = "rgba(66, 66, 66, 0.9)";
       foreground = "rgba(255, 255, 255, 1)";
+      bar-bg-color = "rgba(0, 0, 0, 1)";
       y-offset = 0.95;
+      border-radius = 20; # px
     };
   };
 }
