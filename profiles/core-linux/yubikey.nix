bd12ac657bbcfe9a2aa712a43c77873877632318
Nicolas Berbiche <nicolas@normie.dev>
2022-03-04 20:54:29 -0500

    yubikey: set u2fAuth and yubicoAuth to false for all PAM services

--- profiles/core-linux/yubikey.nix
+++ profiles/core-linux/yubikey.nix
@@ -1,33 +1,33 @@
 { config, pkgs, lib, ... }:
 
 {
-  security.pam.u2f.enable = true;
-  security.pam.u2f.interactive = false;
-  security.pam.u2f.cue = true;
-  security.pam.u2f.control = "sufficient";
-  security.pam.u2f.appId = "pam://${config.networking.hostName}";
-
-  # Disable u2fAuth for all these modules
-  # I only want to enable u2fAuth for ssh, sudo and su really
-  security.pam.services = {
-    chpasswd.u2fAuth = false;
-    i3lock.u2fAuth = false;
-    i3lock-color.u2fAuth = false;
-    lightdm.u2fAuth = false;
-    lightdm-autologin.u2fAuth = false;
-    lightdm-greeter.u2fAuth = false;
-    login.u2fAuth = false;
-    passwd.u2fAuth = false;
-    runuser.u2fAuth = false;
-    runuser-l.u2fAuth = false;
-    swaylock.u2fAuth = false;
-    xlock.u2fAuth = false;
-    vlock.u2fAuth = false;
-    xscreensaver.u2fAuth = false;
+  # Thanks to type merging, the default value of u2fAuth can be set to false
+  # for all pam modules :)
+  options.security.pam.services = with lib; mkOption {
+    type = types.attrsOf (types.submodule {
+      # Instead of overriding options.*.default, set it in the config section of the module
+      config.u2fAuth = mkDefault false;
+      config.yubicoAuth = mkDefault false;
+    });
   };
 
-  home-manager.sharedModules = [{
-    # Get notifications when waiting for my yubikey
-    services.yubikey-touch-detector.enable = true;
-  }];
+  config = {
+    security.pam.u2f.enable = true;
+    security.pam.u2f.interactive = false;
+    security.pam.u2f.cue = true;
+    security.pam.u2f.control = "sufficient";
+    security.pam.u2f.appId = "pam://${config.networking.hostName}";
+
+    security.pam.services = {
+      su.u2fAuth = true;
+      sudo.u2fAuth = true;
+      sshd.u2fAuth = true;
+      polkit-1.u2fAuth = true;
+    };
+
+    home-manager.sharedModules = [{
+      # Get notifications when waiting for my yubikey
+      services.yubikey-touch-detector.enable = true;
+    }];
+  };
 }
