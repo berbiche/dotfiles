32056026093d4bf427037bfc53285e42a5df223d
Nicolas Berbiche <nicolas@normie.dev>
2022-04-12 17:52:43 -0400

    ulauncher: add patch for libx11.so

--- overlays/ulauncher.nix
+++ overlays/ulauncher.nix
@@ -1,5 +1,6 @@
 final: prev: {
   ulauncher = prev.ulauncher.overrideAttrs (drv: rec {
+    name = "${drv.pname}-${drv.version}";
     version = "5.14.2";
     src = prev.fetchurl {
       url = "https://github.com/Ulauncher/Ulauncher/releases/download/${version}/ulauncher_${version}.tar.gz";
@@ -9,6 +10,10 @@ final: prev: {
       ./ulauncher/0001-Adjust-get_data_path-for-NixOS.patch
       ./ulauncher/fix-extensions.patch
       ./ulauncher/fix-path.patch
+      (prev.substituteAll {
+        src = ./ulauncher/fix-libx11-path.patch;
+        libx11_path = "${prev.xorg.libX11}/lib/libX11${prev.stdenv.hostPlatform.extensions.sharedLibrary}";
+      })
     ];
   });
 }
