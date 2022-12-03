f296b33cc9adad2f2f8dbc74d3ab2d30d1b23c4c
Nicolas Berbiche <nicolas@normie.dev>
2022-12-03 16:24:07 -0500

    overlays: gtklock: 2022-08-12 -> 2022-12-01

--- overlays/gtklock.nix
+++ overlays/gtklock.nix
@@ -1,57 +1,17 @@
 final: prev:
-with prev.lib; {
-  gtklock = flip prev.callPackage { } (
-    { stdenv
-    , fetchFromGitHub
-    , pkg-config
-    , gtk3
-    , glib
-    , wayland
-    , wayland-protocols
-    , gtk-layer-shell
-    , wrapGAppsHook
-    , pam
-    , scdoc
-    }:
-    stdenv.mkDerivation rec {
-      pname = "gtklock";
-      version = "unstable-2022-08-12";
+{
+  gtklock = prev.gtklock.overrideAttrs(drv: {
+    version = "2022-12-01";
 
-      src = fetchFromGitHub {
-        owner = "jovanlanik";
-        repo = pname;
-        rev = "e67be9ff0c1c33ec04e8e8d96ebc965972a9bb38";
-        hash = "sha256-cwV6yCSKwIM5V8VvoMsj5W50cvfBAAvSrgLhSkfAYR4=";
-      };
+    src = prev.fetchFromGitHub {
+      owner = "jovanlanik";
+      repo = drv.pname;
+      rev = "1c2a5d36077b9aa92a50ba8483892fb2393be8b4";
+      hash = "sha256-wz23Fg0L3FYaef6fqLhGBTpnk3PDfqW3GtZLp6Tz1cE=";
+    };
 
-      nativeBuildInputs = [
-        pkg-config
-        wrapGAppsHook
-        scdoc
-      ];
-
-      buildInputs = [
-        gtk3
-        glib
-        wayland
-        wayland-protocols
-        gtk-layer-shell
-        pam
-      ];
-
-      makeFlags = [ "DESTDIR=${placeholder "out"}" "PREFIX=" ];
-
-      postInstall = ''
-        rm -rf $out/etc/pam.d
-      '';
-
-      meta = {
-        homepage = "https://github.com/jovanlanik/gtklock";
-        description = "A GTK-based lockscreen for Wayland";
-        platforms = platforms.linux;
-        license = licenses.gpl3;
-        maintainers = [ maintainers.berbiche ];
-      };
-    }
-  );
+    nativeBuildInputs = drv.nativeBuildInputs ++ [
+      prev.wrapGAppsHook
+    ];
+  });
 }
