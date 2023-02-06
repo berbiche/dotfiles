fd09f13bc15f9c95a61a58726ae4acd49b7356f5
Nicolas Berbiche <nicolas@normie.dev>
2023-02-06 13:53:54 -0500

    darwin: replace scroll-reverser with linearmouse

--- profiles/core-darwin/homebrew.nix
+++ profiles/core-darwin/homebrew.nix
@@ -20,6 +20,6 @@
     "monitorcontrol"        # DDC control of external displays (brightness, volume, etc.)
     "raycast"               # Spotlight alternative that works with the /nix/store apps
     "rectangle"             # Window-manager on top of Aqua
-    "scroll-reverser"       # Inverse scroll for external mouse
+    "linearmouse"           # Inverse scroll for external mouse
   ];
 }
