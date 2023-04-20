feb84ecabf820e2512c84b321a4b1b2c1727cfd1
Nicolas Berbiche <nicolas@normie.dev>
2023-04-20 14:19:13 -0400

    dev: export SOPS_AGE_KEY_FILE in session variables

--- profiles/secrets/home-manager.nix
+++ profiles/secrets/home-manager.nix
@@ -3,14 +3,17 @@
 let
   inherit (pkgs.stdenv.hostPlatform) isLinux isDarwin isAarch64;
   osConfig = args.osConfig or { };
+
+  sopsKeyFile = "${config.xdg.configHome}/sops/age/keys.txt";
 in
 lib.mkMerge [
   {
     # Create folder, I don't care if the folder is 755, as long as the content
     # of the secrets themselves is 400
     xdg.configFile."sops/age/.keep".text = "Managed by Home Manager.";
-    sops.age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+    sops.age.keyFile = sopsKeyFile;
     sops.age.generateKey = false;
+    home.sessionVariables.SOPS_AGE_KEY_FILE = sopsKeyFile;
 
 
     # home.file.".gnupg/gpg-agent.conf".text = lib.mkAfter ''
