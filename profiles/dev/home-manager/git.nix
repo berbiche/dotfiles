57b6b4f1c8c2286c25736500573757e33efc9231
Nicolas Berbiche <nicolas@normie.dev>
2023-06-12 15:10:47 -0400

    git: add log graph aliases found on SO

--- profiles/dev/home-manager/git.nix
+++ profiles/dev/home-manager/git.nix
@@ -104,7 +104,18 @@ lib.mkMerge [
         # so just assume that .git is always relative to this alias
         # This alias runs git commit with the content of the last COMMIT_EDITMSG
         # when for instance I enter the wrong password for my pgp key
+        # The cleanup doesn't work correctly and the file changelog is committed as part of the commit.
         fuck = "commit --cleanup=strip -F .git/COMMIT_EDITMSG";
+
+        # Copied from a coworker who probably copied it from here: https://stackoverflow.com/a/34467298
+        lg = lg1;
+        lg1 = "lg1-specific --all";
+        lg2 = "lg2-specific --all";
+        lg3 = "lg3-specific --all";
+        lg1-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+        lg2-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+        lg3-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'";
+
         p = "pull --prune";
         pl = "${p} --rebase";
         pu = "${p} upstream";
