c3e26e905057fd68a9a6439d6a4cccd1a6f6faf3
Nicolas Berbiche <nicolas@normie.dev>
2023-07-19 10:18:20 -0400

    dev: show hidden files with FZF's <C-t>

--- profiles/dev/home-manager/fzf.nix
+++ profiles/dev/home-manager/fzf.nix
@@ -4,9 +4,8 @@
   # ctrl-t, ctrl-r, kill <tab><tab>
   programs.fzf = {
     enable = true;
-    enableZshIntegration = true;
-    enableFishIntegration = true;
-    defaultCommand = ''${pkgs.fd}/bin/fd --follow --type f --exclude=".git" .'';
+    defaultCommand = ''${pkgs.fd}/bin/fd --strip-cwd-prefix -H --follow --type f --exclude=".git"'';
     defaultOptions = [ "--exact" "--cycle" "--layout=reverse" ];
+    fileWidgetCommand = config.programs.fzf.defaultCommand;
   };
 }
