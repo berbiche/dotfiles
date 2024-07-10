ef18b2cb2e6db0a3b924b7eba48a7227caf983ca
Nicolas Berbiche <nicolas@normie.dev>
2024-07-10 16:01:44 -0400

    neovim: update neovide setting name

--- profiles/dev/home-manager/neovim/neovide.lua.nix
+++ profiles/dev/home-manager/neovim/neovide.lua.nix
@@ -2,7 +2,7 @@
 if vim.g.neovide then
   vim.o.guifont = 'Source Code Pro:h14'
   vim.g.neovide_input_use_logo = false
-  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_input_macos_option_key_is_meta = true
   vim.g.neovide_cursor_animation_length = 0
 
   vim.g.neovide_scale_factor = 1.0
